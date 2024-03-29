023a2a5bd902e4b52f3d056694c9ee999028b6f8
Juan <juan.villaverde.rodriguez@udc.es>
2024-03-29 23:52:00 +0100

    merge2

---
+++ Model/Services/Exceptions/MistakenCredentialsException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.Services.Exceptions
+{
+    class MistakenCredentialsException : Exception
+    {
+        // Excepcion con mensaje por defecto
+        public MistakenCredentialsException() : base("Password is not correct. Try again.")
+        {
+        }
+        // Excepcion con mensaje personalizado
+
+        public MistakenCredentialsException(string message) : base(message)
+        {
+        }
+        // Excepcion con mensaje personalizado y excepcion interna
+
+        public MistakenCredentialsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Model/Services/Exceptions/MistakenPasswordException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.Services.Exceptions
+{
+    class MistakenPasswordException : Exception
+    {
+        // Excepcion con mensaje por defecto
+        public MistakenPasswordException() : base("Password is not correct. Try again.")
+        {
+        }
+        // Excepcion con mensaje personalizado
+
+        public MistakenPasswordException(string message) : base(message)
+        {
+        }
+        // Excepcion con mensaje personalizado y excepcion interna
+
+        public MistakenPasswordException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
