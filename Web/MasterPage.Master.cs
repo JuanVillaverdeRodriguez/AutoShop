28027eabf9f2e3926c37e2b8b24034d533f5ab89
dferreirovillamor <daniel.ferreiro.villamor@gmail.com>
2024-05-09 03:10:40 +0200

    cambios en el master page

--- Web/MasterPage.Master.cs
+++ Web/MasterPage.Master.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace Es.Udc.DotNet.PracticaMaD.Web
@@ -11,7 +13,18 @@ namespace Es.Udc.DotNet.PracticaMaD.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (SessionManager.IsUserAuthenticated(Context))
+            {
+                // Obtener una referencia al control de enlace usando su ID
+                HtmlGenericControl sesionLink = (HtmlGenericControl)FindControl("Sesion");
 
+                // Verificar si se encontró el elemento
+                if (sesionLink != null)
+                {
+                    // Hacer que el enlace no sea visible
+                    sesionLink.Style["display"] = "none";
+                }
+            }
         }
     }
 }
